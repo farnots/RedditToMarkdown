042fed8c12c7aa2ff0b88e715c09c47e6128c74d
Lucas Tarasconi <lucas@tarasconi.fr>
2019-08-26 17:50:13 +0200

    Add Bootstrap style and divide script

---
+++ script.js
@@ -0,0 +1,71 @@
+const http = new XMLHttpRequest()
+var data
+var output = ''
+
+function fetchData(url) {
+  output = ''
+  http.open("GET", `${url}.json`)
+  http.responseType = 'json';
+
+  http.send()
+
+  http.onload = function() {
+    data = http.response;
+    const post = data[0].data.children[0].data
+    const comments = data[1].data.children
+    displayTitle(post)
+    output += "\n\n## Comments\n\n"
+    comments.forEach(displayComment);
+
+    console.log("Done")
+    download(output, 'output.md', 'text/plain')
+  }
+}
+function startExport() {
+  console.log("Start exporting")
+  var url = document.getElementById('url').value
+  if (url) {
+    fetchData(url)
+  } else {
+    console.log("No url provided")
+  }
+}
+
+function download(text, name, type) {
+  var a = document.getElementById("a");
+  a.removeAttribute("disabled");
+  var file = new Blob([text], {type: type});
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+}
+
+function displayTitle(post) {
+  output += `# ${post.title}\n`
+  if (post.selftext) {
+    output += `\n${post.selftext}\n`
+  }
+  output += `\n[permalink](http://reddit.com${post.permalink})`
+  output += `\nby *${post.author}* (↑ ${post.ups}/ ↓ ${post.downs})`
+}
+
+function displayComment(comment, index) {
+  depthTag = '─'.repeat(comment.data.depth)
+  if (depthTag != '') {
+    output += `├${depthTag} `
+  } else {
+    output += `##### `
+  }
+  if (comment.data.body) {
+    output += `${comment.data.body} ⏤ by *${comment.data.author}* (↑ ${comment.data.ups}/ ↓ ${comment.data.downs})\n`
+  } else {
+    output += 'deleted \n'
+  }
+
+  if (comment.data.replies) {
+    const subComment = comment.data.replies.data.children
+    subComment.forEach(displayComment)
+  }
+  if (comment.data.depth == 0 && comment.data.replies) {
+    output += '└────\n\n'
+  }
+}
